c070b190b4dd464f4d5d406941cab7f3664d3752
Doug Mahugh <dmahugh@microsoft.com>
2019-03-10 12:04:16 -0700

    rename build script

--- build-windows-exe.bat
+++ build-windows-exe.bat
@@ -1,1 +1,2 @@
+REM command line options for building Windows executable
 pyinstaller cli.py --name opcinfo --onefile --exclude-module=pytest --add-data venv\Lib\site-packages\importlib_resources\version.txt;importlib_resources --add-data opcreader\config.json;opcreader
